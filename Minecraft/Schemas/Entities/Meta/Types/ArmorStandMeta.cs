7d2add427d26d7a3d0c8c8996f0d791074b1a8c7
terminator513 <ctratilatov66@gmail.com>
2026-05-27 02:04:54 +0300

    Split ArmorStandState and ArmorStandMeta

--- Minecraft/Schemas/Entities/Meta/Types/ArmorStandMeta.cs
+++ Minecraft/Schemas/Entities/Meta/Types/ArmorStandMeta.cs
@@ -2,14 +2,6 @@
 
 namespace Minecraft.Schemas.Entities.Meta.Types;
 
-[Flags]
-public enum ArmorStandState : sbyte {
-    None = 0x00,
-    Small = 0x01,
-    Arms = 0x04,
-    NoBasePlate = 0x08,
-    Marker = 0x10
-}
 public record ArmorStandMeta(
     ArmorStandState? State = null,
     Vec3<float>? HeadRotation = null,
